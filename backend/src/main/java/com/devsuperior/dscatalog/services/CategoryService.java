638fdd35ec2bc6f206b0e83ea590521d3ff4032a
Marthiins <sergio.marthiins@gmail.com>
2023-05-15 15:55:36 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,10 +1,12 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
@@ -15,8 +17,10 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 
-	public List<Category> findAll(){ //Metodo acessar o repository e acessar la no banco de dados a Category
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){ //Metodo acessar o repository e acessar la no banco de dados a Category
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x))
+				.collect(Collectors.toList());//Convertendo lista de categoria para CategoriaDTO com lambda
 	
 	}
 
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+Insert into tb_category(name) Values('Books');
+Insert into tb_category(name) Values('Eletronics');
+Insert into tb_category(name) Values('Computers');
